d51b0f93ae79504a8464adc2358c567f1f7d5a44
dpollozhani <38571830+dpollozhani@users.noreply.github.com>
2020-03-26 16:49:02 +0100

    test commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from AnalyzeDocument import AnalyzeDocument
 from pathlib import Path
 
-path = Path('C:/Users/cemil/Desktop')
+string = 'C:\Users\drilo\Vimur AB\Vimur - Documents\Kunder och sälj\_Säljmaterial\Inphinity'
+path = Path(string.replace('\', '/'))
 file = path / 'Recommended_SDV_settings.docx'
 
 document = AnalyzeDocument(file)
